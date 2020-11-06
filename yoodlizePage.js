170548fd5e8dbd69d378cafb8c516b8850031fef
Adillie4 <73258065+Adillie4@users.noreply.github.com>
2020-11-06 09:27:07 -0600

    Add files via upload

---
+++ yoodlizePage.js
@@ -0,0 +1,183 @@
+
+module.exports = {
+    url: 'https://www.yoodlize.com/',
+    elements: {
+        seeAllRV: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]',
+            locateStrategy: 'xpath'
+        },
+        resultTextRV: {
+            selector: '//div[contains(text(),"Recreational Vehicles")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllOG: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[2]',
+            locateStrategy: 'xpath'
+        },
+        resultOG: {
+            selector: '//div[contains(text(),"Outdoor Gear")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllE: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[3]',
+            locateStrategy: 'xpath'
+        },
+        resultE: {
+            selector: '//div[contains(text(),"Electronics")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllPW: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[4]',
+            locateStrategy: 'xpath'
+        },
+        resultPW: {
+            selector: '//div[contains(text(),"Party & Wedding Equipment")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllT: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[5]',
+            locateStrategy: 'xpath'
+        },
+        resultT: {
+            selector: '//div[contains(text(),"Tools")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllC: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[6]',
+            locateStrategy: 'xpath'
+        },
+        resultC: {
+            selector: '//div[contains(text(),"Clothing")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllHK: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[7]',
+            locateStrategy: 'xpath'
+        },
+        resultHK: {
+            selector: '//div[contains(text(),"Home and Kitchen")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllTG: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[8]',
+            locateStrategy: 'xpath'
+        },
+        resultTG: {
+            selector: '//div[contains(text(),"Toys and Games")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllLG: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[9]',
+            locateStrategy: 'xpath'
+        },
+        resultLG: {
+            selector: '//div[contains(text(),"Lawn and Garden")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllSE: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[10]',
+            locateStrategy: 'xpath'
+        },
+        resultSE: {
+            selector: '//div[contains(text(),"Sporting Equipment")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllD: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[11]',
+            locateStrategy: 'xpath'
+        },
+        resultD: {
+            selector: '//div[contains(text(),"DVDs")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllV: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[12]',
+            locateStrategy: 'xpath'
+        },
+        resultV: {
+            selector: '//div[contains(text(),"Venues")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllM: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[13]',
+            locateStrategy: 'xpath'
+        },
+        resultM: {
+            selector: '//div[contains(text(),"Music")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllBE: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[14]',
+            locateStrategy: 'xpath'
+        },
+        resultBE: {
+            selector: '//div[contains(text(),"Business Equipment")]',
+            locateStrategy: 'xpath'
+        },
+        seeAllMi: {
+            selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[15]',
+            locateStrategy: 'xpath'
+        },
+        resultMi: {
+            selector: '//div[contains(text(),"Misc")]',
+            locateStrategy: 'xpath'
+        },
+        icon1: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[1]',
+            locateStrategy: 'xpath'
+        },
+        icon2: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[2]',
+            locateStrategy: 'xpath'
+        },
+        icon3: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[3]',
+            locateStrategy: 'xpath'
+        },
+        icon4: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[4]',
+            locateStrategy: 'xpath'
+        },
+        icon5: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[5]',
+            locateStrategy: 'xpath'
+        },
+        icon6: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[6]',
+            locateStrategy: 'xpath'
+        },
+        icon7: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[7]',
+            locateStrategy: 'xpath'
+        },
+        icon8: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[8]',
+            locateStrategy: 'xpath'
+        },
+        icon9: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[9]',
+            locateStrategy: 'xpath'
+        },
+        icon10: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[10]',
+            locateStrategy: 'xpath'
+        },
+        icon11: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[11]',
+            locateStrategy: 'xpath'
+        },
+        icon12: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[12]',
+            locateStrategy: 'xpath'
+        },
+        icon13: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[13]',
+            locateStrategy: 'xpath'
+        },
+        icon14: {
+            selector: '(//div[@class="h-125 p-s f f-rows f-start-center bg-offWhite"])[14]',
+            locateStrategy: 'xpath'
+        },
+
+    }
+}
