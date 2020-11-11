768b4ec21569ee6adaab8869c9f888c7f3ab3a9d
Adillie4 <73258065+Adillie4@users.noreply.github.com>
2020-11-10 19:41:30 -0600

    Create hloanPage.js
    Home Loan Wizard pageObject

---
+++ hloanPage.js
@@ -0,0 +1,37 @@
+module.exports = {
+    url: 'http://localhost:3000/#/',
+    elements: {
+        //
+        home: '.home-btn',
+        loan: '#loanTypes',
+        property: '#propertyTypes',
+        // nxtBtn: '.margin-btn',
+        // city: 'input[name="city"]',
+        // nxtBtn2: '.wTwo-btn',
+        phBtn: 'button[value="Primary Home"]',
+        rpBtn: 'button[value="Rental Property"]',
+        shBtn: 'button[value="Secondary Home"]',
+        yesBtn: 'button[name="yesButton"]',
+        noBtn: 'button[name="noButton"]',
+        phBtn: 'button[value="Primary Home"]',
+        estPprice: 'input[name="price"]',
+        downP: 'input[name="down"]',
+        masterNxt: 'button[name="nextButton"]',
+        excellent: 'button[value="excellent"]',
+        good: 'button[value="good"]',
+        fair: 'button[value="fair"]',
+        poor: 'button[value="poor"]',
+        bankRno: 'button[value="Has never been in bankruptcy"]',
+        bankR: 'button[value="Has had bankruptcy before"]',
+        fClose: 'button[value="Has had a foreclosure before"]',
+        both: 'button[value="Has had both a foreclosure and a bankruptcy"]',
+        // address: '#addressOne',
+        // address2: '#addressTwo'
+        send: 'button[name="sendButton"]',
+        startAgain: 'button[name="restartButton"]',
+        // formN: '.overarching-div',
+        formLT: 'p[class="loanType p2"]',
+        formPT: 'p[class="propertyType p2"]'
+
+    }
+}
